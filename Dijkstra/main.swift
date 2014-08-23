c1e3e36187433e846b0b8e20c4d3ea19890d45ff
joe <macbook@macbooks-MacBook-2.local>
2014-08-23 09:34:56 -0400

    Initial Commit

---
+++ Dijkstra/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Dijkstra
+//
+//  Created by joe on 8/23/14.
+//  Copyright (c) 2014 Joseph Liccini. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
